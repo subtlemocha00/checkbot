850e279af7b597186c04f2ab459e1d89f6b7e991
subtlemocha <kturley@gmail.com>
2022-04-16 21:21:14 -0400

    added a gif bot
    type #gif followed by a string
    the bot will send a gif message with the top result matching the string

--- checkbot.js
+++ checkbot.js
@@ -1,16 +1,20 @@
 // Setup our environment variables via dotenv
 require("dotenv").config();
+const fetch = (...args) =>
+  import("node-fetch").then(({ default: fetch }) => fetch(...args));
+
 // Import relevant classes from discord.js
 // selected in the bot tab of Dev Portal
 const { Client, Intents } = require("discord.js");
+
 // Instantiate a new client with some necessary parameters.
 const client = new Client({
   intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
 });
-// Notify progress
 client.on("ready", function (e) {
   console.log(`Logged in as ${client.user.tag}!`);
 });
+
 // Authenticate
 client.login(process.env.DISCORD_TOKEN);
 
@@ -21,10 +25,10 @@ client.on("messageCreate", function (msg) {
   if (msg.author.bot) return; // don't reply to other bots
   if (!msg.content.startsWith(prefix)) return;
   const commandBody = msg.content.slice(prefix.length);
-  const args = commandBody.split(" ");
-  const command = args.shift().toLowerCase(); //returns first element of array (i.e. the command)
-  const lowerCaseArgs = [];
-  args.forEach((arg) => lowerCaseArgs.push(arg.toLowerCase()));
+  const tokens = commandBody.split(" ");
+  const command = tokens.shift().toLowerCase(); //returns first element of array (i.e. the command)
+  const lowerCaseTokens = [];
+  tokens.forEach((token) => lowerCaseTokens.push(token.toLowerCase()));
 
   if (command === "help") {
     msg.reply("!help, !check, !hello. That is all so far.");
@@ -33,7 +37,7 @@ client.on("messageCreate", function (msg) {
     msg.reply("yourself, before you wreck yourself!");
   }
   if (command === "hello") {
-    if (lowerCaseArgs.includes("checkbot")) {
+    if (lowerCaseTokens.includes("checkbot")) {
       msg.reply("hey, you there in the bushes");
     } else {
       msg.reply("sup?");
@@ -41,8 +45,21 @@ client.on("messageCreate", function (msg) {
   }
 });
 const prefixB = "#";
-client.on("messageCreate", function (msg) {
+client.on("messageCreate", async (msg) => {
   if (msg.author.bot) return; // don't reply to other bots
-  if (!msg.content.startsWith(prefixB)) return;
-  
+  const tokens = msg.content.split(" ");
+
+  // if the first word is #gif, fetch gif results with following words
+  if (tokens[0] === "#gif") {
+    // combines the words after #gif into a long string, no spaces
+    const keywords = tokens.slice(1, tokens.length).join(" ");
+    // this combo of words is added to create the api endpoint
+    const url = `https://api.tenor.com/v1/search?q=${keywords}&key=${process.env.TENOR_KEY}&limit=10`;
+
+    // fetch the results
+    const response = await fetch(url);
+    const result = await response.json();
+    // const index = Math.floor(Math.random() * result.results.length);
+    msg.channel.send(result.results[0].url);
+  }
 });
